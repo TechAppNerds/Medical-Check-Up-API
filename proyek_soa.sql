da1041f4b742c9164a2f67fbb3b6a11d8c782379
daharyu <dharison05@gmail.com>
2021-07-03 16:34:19 +0700

    update user

--- proyek_soa.sql
+++ proyek_soa.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 24 Jun 2021 pada 13.16
--- Versi server: 10.4.6-MariaDB
--- Versi PHP: 7.2.22
+-- Generation Time: Jul 03, 2021 at 11:33 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -27,11 +26,10 @@ USE `proyek_soa`;
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `client`
+-- Table structure for table `client`
 --
 
-DROP TABLE IF EXISTS `client`;
-CREATE TABLE IF NOT EXISTS `client` (
+CREATE TABLE `client` (
   `email` varchar(50) NOT NULL,
   `username` varchar(50) NOT NULL,
   `name` varchar(50) NOT NULL,
@@ -39,38 +37,52 @@ CREATE TABLE IF NOT EXISTS `client` (
   `tanggal_lahir` date NOT NULL,
   `no_telp` int(15) NOT NULL,
   `saldo` int(10) NOT NULL,
-  PRIMARY KEY (`email`)
+  `role` varchar(20) NOT NULL COMMENT 'dokter/client/receptionist'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `client`
+-- Dumping data for table `client`
 --
 
-INSERT INTO `client` (`email`, `username`, `name`, `password`, `tanggal_lahir`, `no_telp`, `saldo`) VALUES
-('abc@d.com', 'bca', 'bca', '$2a$10$KC.qyauUwQkWj1oDF78rPebSknDN3/TJeGafUYtjs3bztn6aX3/DK', '1990-10-10', 1990122, 0);
+INSERT INTO `client` (`email`, `username`, `name`, `password`, `tanggal_lahir`, `no_telp`, `saldo`, `role`) VALUES
+('abc@d.com', 'bca', 'bca', '$2a$10$KC.qyauUwQkWj1oDF78rPebSknDN3/TJeGafUYtjs3bztn6aX3/DK', '1990-10-10', 1990122, 0, 'dokter');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `developer`
+-- Table structure for table `developer`
 --
 
-DROP TABLE IF EXISTS `developer`;
-CREATE TABLE IF NOT EXISTS `developer` (
+CREATE TABLE `developer` (
   `email` varchar(100) NOT NULL,
   `username` varchar(20) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`email`),
-  UNIQUE KEY `username` (`username`)
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `developer`
+-- Dumping data for table `developer`
 --
 
 INSERT INTO `developer` (`email`, `username`, `name`, `password`) VALUES
 ('appgeeks@gmail.com', 'appnerds', 'Edwin Lo', '$2a$10$IaBQ5TqXKcKiDv9amQUbJ.vplitUGcgNaRpvcL91rt1yUaMPMwYJ2');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `developer`
+--
+ALTER TABLE `developer`
+  ADD PRIMARY KEY (`email`),
+  ADD UNIQUE KEY `username` (`username`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
